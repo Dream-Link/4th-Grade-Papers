1feb7919e435bfdc4213bd8b1d81944d6b9c6dca
Dream-Link <kavitacloud7@gmail.com>
2025-09-27 22:43:19 +0530

    service-worker.js

--- sw.js
+++ sw.js
@@ -1,60 +1,48 @@
-// Define a cache name for your app's assets.
-const CACHE_NAME = '4th Grade Paper-v1';
-
-// List of files to cache for offline use.
-const URLS_TO_CACHE = [
-    '/',
-    './index.html',
-    './questions-data.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css',
-    'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js',
-    './icons/icon-192x192.png',
-    './icons/icon-512x512.png'
+const CACHE_NAME = '4th-Grade-Anlysis-by-MP-SALODIYA-v1';
+// Files to cache for offline use
+const urlsToCache = [
+  '/',
+  '/index.html'
 ];
 
-// Install event: triggered when the service worker is first installed.
+// Install a service worker
 self.addEventListener('install', event => {
-    // Perform install steps
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Opened cache');
-                // Add all the specified URLs to the cache.
-                return cache.addAll(URLS_TO_CACHE);
-            })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-// Fetch event: triggered for every network request made by the page.
+// Cache and return requests
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                // If the request is found in the cache, return the cached response.
-                if (response) {
-                    return response;
-                }
-                // If the request is not in the cache, fetch it from the network.
-                return fetch(event.request);
-            })
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
 });
 
-// Activate event: triggered when the service worker is activated.
-// This is a good place to clean up old caches.
+// Update a service worker
 self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    // If a cache is not in our whitelist, delete it.
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
 });
-
