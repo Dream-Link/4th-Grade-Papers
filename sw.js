98b30d1b91fa4dded984f4eef66db0db071c1ff3
Dream-Link <kavitacloud7@gmail.com>
2025-11-30 15:34:20 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,49 +1,10 @@
+const CACHE_NAME = "4thgrade-v1";
+const ASSETS = ["/", "/index.html", "/style.css", "/script.js"];
 
-const CACHE_NAME = '4th-Grade-Anlysis-by-MP-SALODIYA-v1';
-const urlsToCache = [
-  '/',
-  '/index.html'
-];
-
-// 1. Install Event: Cache the core files
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// 2. Fetch Event: Serve from cache first, then network
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        // Not in cache - fetch from network
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-// 3. Activate Event: Clean up old caches
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
